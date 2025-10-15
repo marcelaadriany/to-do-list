2813a598c7e6a7d3ea121df77570fcf247496ced
marcelaadriany <marcela.adriany@gmail.com>
2025-10-15 16:39:47 -0300

    creates task service

--- src/main/java/com/marcela/todo/repository/TaskRepository.java
+++ src/main/java/com/marcela/todo/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.marcela.todo.repository;
 
-public class TaskRepository {
+import com.marcela.todo.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+  // aqui eu posso criar métodos customizados
+  // exemplo: List<Task> findByDoneFalse(); busca as tasks que não foram finalizadas
 }
---
+++ src/main/java/com/marcela/todo/service/TaskService.java
@@ -0,0 +1,5 @@
+package com.marcela.todo.service;
+
+public class TaskService {
+
+}
